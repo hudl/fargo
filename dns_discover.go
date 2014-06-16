2dd59e003378f31c85702266395e159b179202b8
Ryan S. Brown <sb@ryansb.com>
2014-06-16 10:17:53 -0500

    return the TTL of the region record for expiry purposes

--- dns_discover.go
+++ dns_discover.go
@@ -14,21 +14,21 @@ const azURL = "http://169.254.169.254/latest/meta-data/placement/availability-zo
 var ErrNotInAWS = fmt.Errorf("Not in AWS")
 
 func discoverDNS(domain string) (servers []string, ttl time.Duration, err error) {
-	// all DNS queries must use the FQDN
+	r, _ := region()
 
-	z, _ := availabilityZone()
-	domain = "txt." + z + "." + dns.Fqdn(domain)
+	// all DNS queries must use the FQDN
+	domain = "txt." + r + "." + dns.Fqdn(domain)
 	if _, ok := dns.IsDomainName(domain); !ok {
 		err = fmt.Errorf("invalid domain name: '%s' is not a domain name", domain)
 		return
 	}
-	availabilityZoneRecords, err := findTXT(domain)
+	regionRecords, ttl, err := findTXT(domain)
 	if err != nil {
 		return
 	}
 
-	for _, az := range availabilityZoneRecords {
-		s, er := findTXT("txt." + dns.Fqdn(az))
+	for _, az := range regionRecords {
+		s, _, er := findTXT("txt." + dns.Fqdn(az))
 		if er != nil {
 			continue
 		}
@@ -37,27 +37,29 @@ func discoverDNS(domain string) (servers []string, ttl time.Duration, err error)
 	return
 }
 
-func findTXT(fqdn string) ([]string, error) {
+func findTXT(fqdn string) ([]string, time.Duration, error) {
+	defaultTTL := 120 * time.Second
 	query := new(dns.Msg)
 	query.SetQuestion(fqdn, dns.TypeTXT)
 	response, err := dns.Exchange(query, dnsServerAddr)
 	if err != nil {
 		log.Error("Failure resolving name %s err=%s", fqdn, err.Error())
-		return nil, err
+		return nil, defaultTTL, err
 	}
 	if len(response.Answer) < 1 {
 		err := fmt.Errorf("no Eureka discovery TXT record returned for name=%s", fqdn)
 		log.Error("no answer for name=%s err=%s", fqdn, err.Error())
-		return nil, err
+		return nil, defaultTTL, err
 	}
 	if response.Answer[0].Header().Rrtype != dns.TypeTXT {
 		err := fmt.Errorf("did not receive TXT record back from query specifying TXT record. This should never happen.")
 		log.Error("Failure resolving name %s err=%s", fqdn, err.Error())
-		return nil, err
+		return nil, defaultTTL, err
 	}
 	txt := response.Answer[0].(*dns.TXT)
+	ttl := response.Answer[0].Header().Ttl
 
-	return txt.Txt, nil
+	return txt.Txt, time.Duration(ttl) * time.Second, nil
 }
 
 var dnsServerAddr string
@@ -68,22 +70,31 @@ func init() {
 	dnsServerAddr = config.Servers[0] + ":" + config.Port
 }
 
+func region() (string, error) {
+	zone, err := availabilityZone()
+	if err != nil {
+		log.Error("Could not retrieve availability zone err=%s", err.Error())
+		return "us-east-1", err
+	}
+	return zone[:len(zone)-1], nil
+}
+
 // defaults to us-east-1 if there's a problem
 func availabilityZone() (string, error) {
 	response, err := goreq.Request{Uri: azURL}.Do()
 	if err != nil {
-		return "us-east-1", err
+		return "", err
 	}
 	if response.StatusCode < 200 || response.StatusCode >= 300 {
 		body, _ := response.Body.ToString()
-		return "us-east-1", fmt.Errorf("bad response code: code %d does not indicate successful request, body=%s",
+		return "", fmt.Errorf("bad response code: code %d does not indicate successful request, body=%s",
 			response.StatusCode,
 			body,
 		)
 	}
 	zone, err := response.Body.ToString()
 	if err != nil {
-		return "us-east-1", err
+		return "", err
 	}
 	return zone[:len(zone)-1], nil
 }
