bf62a0025170652e6fdda37fdc1facc19c4451dc
Ryan S. Brown <sb@ryansb.com>
2015-12-06 15:26:17 -0500

    Add more tests for serializers

--- tests/metadata_test.go
+++ tests/metadata_test.go
@@ -3,6 +3,7 @@ package fargo_test
 // MIT Licensed (see README.md) - Copyright (c) 2013 Hudl <@Hudl>
 
 import (
+	"encoding/xml"
 	"github.com/hudl/fargo"
 	. "github.com/smartystreets/goconvey/convey"
 	"strconv"
@@ -54,6 +55,31 @@ func TestGetFloat(t *testing.T) {
 					So(err, ShouldBeNil)
 					So(actualValue, ShouldEqual, float32(1.9))
 				})
+			})
+		})
+	})
+}
+
+func TestSerializeMeta(t *testing.T) {
+	Convey("Given an instance", t, func() {
+		instance := new(fargo.Instance)
+		Convey("With metadata", func() {
+			instance.SetMetadataString("test", "value")
+			Convey("Serializing results in correct JSON", func() {
+				b, err := instance.Metadata.MarshalJSON()
+				So(err, ShouldBeNil)
+				So(string(b), ShouldEqual, "{\"test\":\"value\"}")
+			})
+			Convey("Serializing results in correct XML", func() {
+				b, err := xml.Marshal(instance.Metadata)
+				So(err, ShouldBeNil)
+				So(string(b), ShouldEqual, "<InstanceMetadata><test>value</test></InstanceMetadata>")
+			})
+			Convey("Blank metadata results in blank XML", func() {
+				metadata := new(fargo.InstanceMetadata)
+				b, err := xml.Marshal(metadata)
+				So(err, ShouldBeNil)
+				So(string(b), ShouldEqual, "<InstanceMetadata></InstanceMetadata>")
 			})
 		})
 	})
