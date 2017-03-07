210dd0bfc1d52b1492d6dbf142dfe8853af2a6ca
Martin Baillie <martin.t.baillie@gmail.com>
2017-03-07 11:53:28 +1100

    Introducing a Logger interface for flexibility.
    The Logger methods at present are on feature parity with Fargo's chosen
    logging library: "github.com/op/go-logging".
    
    Having an interface and setters allows consumers of Fargo to bring their
    own logging solution if desired, so long as they match the behaviours
    stipulated in the interface.

--- log.go
+++ log.go
@@ -6,11 +6,44 @@ import (
 	"github.com/op/go-logging"
 )
 
-var log = logging.MustGetLogger("fargo")
-var metadataLog = logging.MustGetLogger("fargo.metadata")
-var marshalLog = logging.MustGetLogger("fargo.marshal")
+type Logger interface {
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Panic(args ...interface{})
+	Panicf(format string, args ...interface{})
+	Critical(args ...interface{})
+	Criticalf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Warning(args ...interface{})
+	Warningf(format string, args ...interface{})
+	Notice(args ...interface{})
+	Noticef(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+}
+
+var log, metadataLog, marshalLog Logger
+
+func SetLogger(l Logger) {
+	log = l
+}
+
+func SetMarshalLogger(l Logger) {
+	metadataLog = l
+}
+
+func SetMetadataLogger(l Logger) {
+	marshalLog = l
+}
 
 func init() {
+	SetLogger(logging.MustGetLogger("fargo"))
+	SetMarshalLogger(logging.MustGetLogger("fargo.metadata"))
+	SetMetadataLogger(logging.MustGetLogger("fargo.marshal"))
+
 	logging.SetLevel(logging.WARNING, "fargo.metadata")
 	logging.SetLevel(logging.WARNING, "fargo.marshal")
 }
